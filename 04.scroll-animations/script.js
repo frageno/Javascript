520a91c2ebfca461afc3cfc20ffbf796fcaf11d6
Simon <64953895+frageno@users.noreply.github.com>
2022-05-19 10:43:53 +0200

    Scroll animations

---
+++ 04.scroll-animations/script.js
@@ -0,0 +1,19 @@
+// FUNCTION
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    for (let i=0; i < reveals.length; i++){
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active');
+        }else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+// ADD EVENT
+window.addEventListener('scroll', reveal);
